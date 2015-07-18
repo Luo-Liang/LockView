5ad101c142b72ae3c2b20f80eb074be16f6313d1
liangluo <llmainland@live.cn>
2015-07-17 23:59:52 -0700

    update service contract

---
+++ IRequestProxying.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfoView
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IRequestProxying" in both code and config file together.
+    [ServiceContract]
+    public interface IRequestProxying
+    {
+        [OperationContract]
+        [WebInvoke(
+        RequestFormat = WebMessageFormat.Json,
+        ResponseFormat = WebMessageFormat.Json, Method = "POST")]
+        Task<string> FulfillRequestSimple(string requestString);
+    }
+}
---
+++ RequestProxying.svc.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace InfoView
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "RequestProxying" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select RequestProxying.svc or RequestProxying.svc.cs at the Solution Explorer and start debugging.
+    public class RequestProxying : IRequestProxying
+    {
+        static WebClient requestClient = new WebClient();
+        static Regex _htmlRegex = new Regex("<.*?>");
+        public async Task<string> FulfillRequestSimple(string requestString)
+        {
+            var response = await requestClient.DownloadStringTaskAsync(requestString.Trim('"'));
+            response = WebUtility.HtmlDecode(Regex.Unescape(Regex.Replace(response, "<[^>]+>", string.Empty)));
+            return response;
+        }
+    }
+}
