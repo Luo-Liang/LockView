3987a6f3a00aadadee8a1608387dd80e56e33a10
Liang Luo <llmainland@live.cn>
2015-10-17 22:45:21 -0700

    Cosmetic Surgeries. Readying interest setups.

---
+++ LockViewApp.W81/InterestGathererControl.cs
@@ -0,0 +1,15 @@
+﻿using InfoViewApp.WP81.InterestGathering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace LockViewApp.W81
+{
+    public class InterestGathererControl : UserControl
+    {
+        public InterestGatherer Gatherer { get; set; }
+    }
+}
